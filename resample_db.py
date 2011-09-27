2b624c33d6e517b8022c403011a19c099cc46167
Daniel Soto <danielrsoto@gmail.com>
2011-09-27 16:45:41 -0400

    adds handling for empty query returns

--- resample_db.py
+++ resample_db.py
@@ -30,11 +30,16 @@ select timestamp from logs where timestamp>'2011-08-01 00:00:00' and timestamp<'
 
 import sqlite3
 from datetime import datetime
+from datetime import timedelta
 
 db='ml01.db'
 
 #select max(timestamp) from logs where timestamp>='2011-09-08 08:00:00' and timestamp<='2011-09-08 09:00:00' and circuitid=4;
 
+'''
+query the database and return the most recent timestamp
+for a circuit in a certain date range
+'''
 def get_most_recent_timestamp_in_range(circuit,
                                        timeStart=datetime(2011,9,1),
                                        timeEnd=datetime(2011,9,2)):
@@ -50,21 +55,29 @@ def get_most_recent_timestamp_in_range(circuit,
     con.close()
 
     return ts
-
+'''
+return the values for a circuit and timestamp
+'''
 def get_wh_for_timestamp_and_circuit(circuit, timestamp):
     con = sqlite3.connect(db, detect_types=sqlite3.PARSE_COLNAMES)
-    sql = '''select watthours_sc20
+    cur = con.cursor()
+    sql = '''select watthours_today, credit
              from logs
              where circuitid=%s
              and timestamp='%s';
           ''' %(circuit,timestamp)
 
-    for row in con.execute(sql):
+    #for row in con.execute(sql):
+    cur.execute(sql)
+    row = cur.fetchone()
+    if row!=None:
         wh = row[0]
-    con.close()
-    print wh
-    return wh
+        cr = row[1]
+    else:
+        wh = None
+        cr = None
 
+    return wh, cr
 
 datestamps = [datetime(2011,9,1),
               datetime(2011,9,2),
@@ -72,10 +85,24 @@ datestamps = [datetime(2011,9,1),
               datetime(2011,9,4),
               datetime(2011,9,5)]
 
-for i in range(len(datestamps)-1):
+timeStart = datetime(2011,9,2)
+timeEnd   = datetime(2011,9,3)
 
-    for cid in range(1,3):
-        print 'circuitid',
+current_time = timeStart
+sample_period = timedelta(hours = 1)
+
+while current_time < timeEnd:
+    for cid in range(1,2):
+        print current_time,
         print cid,
-        ts = get_most_recent_timestamp_in_range(cid, datestamps[i], datestamps[i+1])
-        get_wh_for_timestamp_and_circuit(cid, ts)
+        ts = get_most_recent_timestamp_in_range(cid, current_time - sample_period, current_time)
+        wh, cr = get_wh_for_timestamp_and_circuit(cid, ts)
+        if wh!=None:
+            last_wh = wh;
+            last_cr = cr;
+        else:
+            print '*',
+            wh = last_wh;
+            cr = last_cr;
+        print wh, cr
+    current_time += sample_period
