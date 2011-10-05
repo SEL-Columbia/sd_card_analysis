c9a233a74fb954f87e81ec740186a9c3c6cd4e7e
Daniel Soto <danielrsoto@gmail.com>
2011-10-05 14:33:34 -0400

    script with query to create daily_watthours table and import csv

---
+++ daily_watthours.sql
@@ -0,0 +1,19 @@
+--this script creates a table in the sql database for
+--daily watthour info
+
+--drop table hourly_watthours;
+
+create table daily_watthours(
+metername string,
+circuitid integer,
+timestamp timestamp,
+watthours float,
+numsamples integer
+);
+
+
+.separator ","
+
+.import daily_watthours.csv daily_watthours
+
+--create index logs_idx on logs (timestamp);
